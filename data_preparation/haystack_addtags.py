470052d8ad23e7feca5d246ecd561afe4fcc8665
Ubuntu <ubuntu@ip-172-31-12-103.eu-west-1.compute.internal>
2024-03-01 19:37:50 +0000

    adding index

--- data_preparation/haystack_addtags.py
+++ data_preparation/haystack_addtags.py
@@ -1,45 +1,22 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[1]:
-
-
-import neo4j
-
-
 # In[2]:
 
 
 import pandas as pd
 
-
-# In[3]:
-
+from neo4j import GraphDatabase
 
 import json
-
+import os
 
 directory = 'path/to/wiki_files'  # Update this to your directory path
-
-
-# In[5]:
 
 
 with open("proto_wiki_data.json") as f:
     relevant = json.load(f)
 
-
-import json
-
-
-
-# In[7]:
-
-
-from neo4j import GraphDatabase
-
-
-# In[8]:
 
 
 driver = GraphDatabase.driver("bolt://"+"localhost"+":"+"7688", auth=("neo4j", "password"))
@@ -61,19 +38,9 @@ def add_tags_node(tx, file_path, tags):
          tx.run(query, file_path = file_path, topic=tag)
 
 
-# In[10]:
-
-
-import os
-
-
-# In[11]:
-
 
 files = [f for f in os.listdir(directory) if f.endswith('.txt')]
 
-
-# In[12]:
 
 
 count=0
@@ -92,8 +59,11 @@ for i, ii in enumerate(relevant):
         pass
 
 
-# In[ ]:
+#building the index
 
+query = "CREATE FULLTEXT INDEX documentContent FOR (n:Document) ON EACH [n.content]"
+with driver.session() as session:
+    session.run(query)
 
 
 
